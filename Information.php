3a81f24482c7b77f67c3c453aff1b88c19a0e949
tundebabs2018@ <babatunde2018@hotmail.com>
2019-12-12 15:44:38 +0100

    first commit

---
+++ Information.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>N-power Project</title>
+  <meta name="description" content="" />
+  <meta name="author" content="Tooplate" />
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Additional CSS Files -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css" />
+  <link rel="stylesheet" href="assets/css/tooplate-style.css" />
+  <link rel="stylesheet" href="assets/css/owl.css" />
+  <link rel="stylesheet" href="assets/css/lightbox.css" />
+</head>
+<!--
+SERIKI S.B
+-->
+<body>
+  <div id="page-wraper">
+    <!-- Sidebar Menu -->
+    <div class="responsive-nav">
+      <i class="fa fa-bars" id="menu-toggle"></i>
+      <div id="menu" class="menu">
+        <i class="fa fa-times" id="menu-close"></i>
+        <div class="container">
+          <div class="image">
+            <a href="#"><img src="assets/images/logo-image.jpg" alt="my logo" /></a>
+          </div>
+          <div class="author-content">
+            <h4>N-POWER <br>Academy</h4>
+            <span>Knowledge is power</span>
+          </div>
+          <nav class="main-nav" role="navigation">
+            <ul class="main-menu">
+              <li><a href="#section1">Our Objective</a></li>
+              <li><a href="#section2">News Section</a></li>
+              <li><a href="#section3">Media and Curriculum Section</a></li>
+              <li><a href="#section4">Contact us</a></li>
+            </ul>
+          </nav>
+          <div class="social-network">
+            <ul class="soial-icons">
+              <li>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-rss"></i></a>
+              </li>
+            </ul>
+          </div>
+          <div class="copyright-text">
+            <p>Copyright 2019 by Babs Telecoms<br>
+          </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="section about-me" data-section="section1">
+      <div class="container">
+      <div class="top-header">
+        <img src="assets/images/aerobic-girls.jpg" alt="aerobic girls" />
+      </div>
+        <div class="section-heading">
+          <marquee bgcolor="blue" behavior="alternate" direction="left">Morning Classes: 8:00am - 10:00am | Afternoon Classes: 1:00pm - 3:00pm | Evening Classes: 5:00pm - 7:00pm</marquee>
+          <center><a>You can kick Start Your Career with us by registering with us: </a></center>
+            <center><div class="white-button">
+              <a href="Signup.php">Sign In</a> &nbsp; &nbsp; &nbsp; &nbsp; 
+              
+                <a href="register.php">Register</a> </center>
+          
+                <h2>Our Objective</h2>
+          <div class="line-dec"></div>
+          <span>We seek to have a positive impact in the lives of the people we serve by always putting our clients, partners and teammates first. We invite perspective, and we consistently celebrate each other’s unique contributions as we work together to bring the best solutions to our clients</span>
+        </div>
+        <div class="right-image-post">
+          <div class="row">            
+              <div class="col-md-4">
+                <div class="left-image">
+                  <img src="assets/images/mountain-reflection.jpg" alt="Mountain Reflection" />
+                </div>
+              </div>
+              
+              <div class="col-md-8">
+                <div class="right-text">
+                  <h3>Our Modern Day Career</h3>
+                  <p>Discover a career in a fast-paced, global environment. N-Power is proud to be recognized in the community as a leader in the promotion and practice of diversity and inclusion. We believe the innovation and success of our organization comes from embracing our differences.</p>
+                  <p>To improve on our work force we educate & Train personel and we provide tools and resources to assist you in building skills to sell and support solutions. Our goal is to help you solve complex training roadmaps so you achieve higher levels of authorization and increase revenue. </p>
+                  
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-services" data-section="section2">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Our News Section</h2>
+            <div class="line-dec"></div>
+            <span>All you need to know about Npower</span>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-area-chart"></i>
+                <h4>Efficient</h4>
+                <p>Npower had a landmark break through by training 50,000 Web Programmers</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-check-square-o"></i>
+                <h4>Trusted</h4>
+                <p>Npower to recuit for personel for the upcoming programs!!!</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-question-circle"></i>
+                <h4>Reliable</h4>
+                <p>Npower to give scholarship to all the best awarding students during the programme</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-support"></i>
+                <h4>We Support you</h4>
+                <p>Npower is ready to  give you the necessary support to achieve your goals in I.T.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-work" data-section="section3">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Multimedia Section</h2>
+            <div class="line-dec"></div>
+            
+          </div>
+         
+                        
+                  <span>* Computer Engineering (COMPTIA A<sup>+</sup>) <br> 
+                    * Networking (N<sup></sup>+</sup>, CCNA and CCNP) <br>
+                     * Oracle Database 11g (OCA and OCP)<br> 
+                      * ICT Fundamentals i.e MS Word, Excel, Access, Powerpoint, Internet Basic e.t.c <br>
+                       * Web Development and Multimedia <br>
+                        * E-Business i.e Customer Relationship Management, Project Management Professional <br>
+                      * Microsoft Server 2019 (MCSC Server 2019, Exchange 2016) <br>
+                       * British Computer Society <br>
+                        * Security i.e Certified Ethical Hacker <br>
+                      * Software Development i.e Microsoft Technology, Java Technology <br>
+                       * Mobile Application Development (ANDRIOD) <br>
+                        * AUTOCAD<br>
+                        * Advanced Certificate in Information Technology <br>
+                        * Honour Diploma in Information Technology <br>
+                         * Advanced Diploma in Information Technology<br> 
+                          * Professional Diploma in Information Technology (Software Engineering / Network Engineering)<br>
+                          * Graduate Diploma in Information Technology<br>
+                        </span>
+    
+
+                      <div class ="container">
+                        <center>
+            <audio controls>
+                        <source src="assets/images/Blues.mp3" source type="audio/mp3">
+
+                    </audio>
+            
+            
+                <video width="600" height="600" controls> 
+                  <source src= "assets/images/vid2.mp4" type="video/mp4">
+                           
+                  </video>            
+                               
+          
+          </center>
+
+</div>
+   
+
+          
+      </section>
+
+      <section class="section contact-me" data-section="section4">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Contact</h2>
+            <div class="line-dec"></div>
+            <span>For all you enquiry please call or visit us!!!</span>
+          </div>
+          <div class="row">
+            <div class="right-content">
+              <div class="container">
+                <form id="contact" action="" method="post">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="name" type="text" class="form-control"
+                          id="name" placeholder="Your name..." required="" />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control"
+                          id="email" placeholder="Your email..." required=""  />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input name="subject" type="text" class="form-control" 
+                          id="subject" placeholder="Subject..." required="" />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" class="form-control"
+                          id="message" placeholder="Your message..." required="" ></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">
+                          Send Message
+                        </button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+      //according to loftblog tut
+      $(".main-menu li:first").addClass("active");
+
+      var showSection = function showSection(section, isAnimate) {
+        var direction = section.replace(/#/, ""),
+          reqSection = $(".section").filter(
+            '[data-section="' + direction + '"]'
+          ),
+          reqSectionPos = reqSection.offset().top - 0;
+
+        if (isAnimate) {
+          $("body, html").animate(
+            {
+              scrollTop: reqSectionPos
+            },
+            800
+          );
+        } else {
+          $("body, html").scrollTop(reqSectionPos);
+        }
+      };
+
+      var checkSection = function checkSection() {
+        $(".section").each(function() {
+          var $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+          if (topEdge < wScroll && bottomEdge > wScroll) {
+            var currentId = $this.data("section"),
+              reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+            reqLink
+              .closest("li")
+              .addClass("active")
+              .siblings()
+              .removeClass("active");
+          }
+        });
+      };
+
+      $(".main-menu").on("click", "a", function(e) {
+        e.preventDefault();
+        showSection($(this).attr("href"), true);
+      });
+
+      $(window).scroll(function() {
+        checkSection();
+      });
+    </script>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+	<title>Registration Page</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="assets/css/styler.css">
+</head>    <body>
+
+      <form action="index.html" method="post">
+      
+        <h1>Registration Page</h1>
+        
+        <fieldset>
+          <legend><span class="number">1</span>Your basic info</legend>
+          <label for="name">Surname:</label>
+          <input type="text" id="name" name="user_name" placeholder="enter your Surname">
+
+          <label for="name">Firstname:</label>
+          <input type="text" id="name" name="user_name" placeholder="enter your Firstname">
+
+          <label for="name">Lastname:</label>
+          <input type="text" id="name" name="user_name" placeholder="enter your Lastname">
+          
+          <label for="mail">Email:</label>
+          <input type="email" id="mail" name="user_email">
+          
+          <label for="name">State Your Preferred Course </label>
+          <input type="text" id="name" name="user_name" placeholder="Preferred Course">
+
+          <label for="Gender">Gender</label>
+          <select id="gender" name="gender">
+            <optgroup label="Select">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              
+            </optgroup>
+            
+
+          
+            <label for="environment" >Is the Environment Friendly with you!!!</label>
+            <select id="environment" name="environment">
+              <optgroup label="Select">
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+                
+              </optgroup>
+            
+        </fieldset>
+
+                
+        <button type="submit">Sign Up</button>
+      </form>
+      
+    </body>
+</html>
